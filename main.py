87e3e736fd3e966073f97f0b0ac78e88af8dba62
pradiptapratama11-netizen <pradiptapratama11@gmail.com>
2026-04-26 23:35:41 +0700

    Implement Telegram message sending functionality
    Add function to send a message via Telegram bot

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import os
 import requests
 
-print("Scanner hidup bro 🚀")
+BOT_TOKEN=os.getenv("BOT_TOKEN")
+CHAT_ID=os.getenv("CHAT_ID")
+
+def send():
+    url=f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    requests.post(
+      url,
+      json={
+        "chat_id":CHAT_ID,
+        "text":"🚀 DAILY MEME PICKS TEST"
+      }
+    )
+
+send()
